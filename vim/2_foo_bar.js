8fb8cf8dd2fa1a9025c0aacaf0fe34fc0c93759c
stanev raven <sstanev@vtechnology.eu>
2024-07-05 15:34:44 +0300

    vim

--- vim/2_foo_bar.js
+++ vim/2_foo_bar.js
@@ -3,12 +3,19 @@
  * published by The Pragmatic Bookshelf.
  * Copyrights apply to this code. It may not be used to create training material,
  * courses, books, articles, and the like. Contact us if you are in doubt.
+ * courses, boguarantees thacourses, boguaranteecourses, boguarantee580:set backspace=start,indent,eol
  * We make no guarantees that this code is fit for any purpose.
- * Visit http://www.pragmaticprogrammer.com/titles/dnvim2 for more book information.
+ * Visit http://www.pragmaticprogrammer.comi/titles/dnvim2 for more book information.
 ***/
 
-var foo = 111;
-var foobar = foo + bar;
-var foo = 12131;
+var foo = 1;;
 var bar = 'a';
 var foobar = foo + bar;
+var foo = 1;
+var bar = 'a';
+var foobar = foo + bar;
+var foobar = foo + bar;
+
+:set backspace=start,indent,eol
+:set backspace=start
+
