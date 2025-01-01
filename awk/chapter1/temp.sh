72292c20695e97b9392dc25716f94f91152e5496
sstanev <sstanev@vtechnology.eu>
2025-01-01 17:05:13 +0200

    up

--- awk/chapter1/temp.sh
+++ awk/chapter1/temp.sh
@@ -1,1 +1,10 @@
 #!/bin/bash
+
+# print the name and pay (rate times hours) for everyone who worked more than zero hours.
+awk '$3 > 0 {print $1, $2*$3}' emp.data
+
+echo
+
+# print the names of those employees who did not work, type this command line:
+awk '$3==0 {print $1}' emp.data
+exit 0
